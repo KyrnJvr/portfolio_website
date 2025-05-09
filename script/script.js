29bb066addc4c300f62d41b7017f7f0a4744f8e9
Kyle Javier <kyrncjvr@gmail.com>
2025-05-09 22:45:10 +0930

    Finished Website Template, working on home page content

---
+++ script/script.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+    loadPage('home'); // load home page by default
+}
+
+
+function loadPage(page){
+    const content = document.getElementById('content');
+    const spinner = document.getElementById('spinner');
+
+    spinner.classList.remove('fade-out');
+
+    fetch(`pages/${page}.html`)
+    .then((res) => {
+        if(!res.ok){
+            throw new Error(`Page not found`);
+        }
+        return res.text();
+    })
+    .then((html) => {
+        content.innerHTML = html;
+        spinner.classList.add('fade-out');
+        
+    })
+    .catch(() => {
+        content.innerHTML = `<h2>Error Loading content</h2><p>Please try again.</p>`;
+        spinner.classList.add('fade-out');
+    });
+}
